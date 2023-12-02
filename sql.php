93ba16e3ea7fbad4423a435834c5c19d143ec805
Rupa sravya <49145304+RupaSravya@users.noreply.github.com>
2023-12-02 20:20:24 +0530

    Update sql.php

--- sql.php
+++ sql.php
@@ -1,12 +1,17 @@
 <?php
-require 'vendor/autoload.php'; // Include the MongoDB PHP driver
+// config.php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'guvi');
 
-$mongoClient = new MongoDB\Client("mongodb://localhost:27017");
+// your php file
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-try {
-    $mongoClient->listDatabases();
-    echo "Connected successfully";
-} catch (MongoDB\Driver\Exception\Exception $e) {
-    echo "Connection failed: " . $e->getMessage();
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+echo "Connected successfully";
 ?>
