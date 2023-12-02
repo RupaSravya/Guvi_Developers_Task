bbc6f93e62fe9d2bf433c66520ad670d1fffd633
Rupa sravya <49145304+RupaSravya@users.noreply.github.com>
2023-12-02 16:41:59 +0530

    Add files via upload

---
+++ mongo.php
@@ -0,0 +1,17 @@
+<?php
+// config.php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'assessment');
+
+// your php file
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+?>
